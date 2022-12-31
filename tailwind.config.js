846f99ef0245afadf896153056b4afb3695dc621
Davy Duperron <yamafaktory@gmail.com>
2022-12-31 16:31:42 +0100

    chore(project): create first version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        jetbrains: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
